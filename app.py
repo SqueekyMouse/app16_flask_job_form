747cf54c592cf3cf79981f0478e04e4f9302ee77
sreejith <squeeky.mouse@gmail.com>
2023-06-11 20:36:54 +0530

    added submit button code

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST']) #url to handle get and post reqs!!!
 def index():
+    # print(request.method)
+    if request.method=='POST': #for submit button!!!
+        first_name=request.form['first_name']
+        last_name=request.form['last_name']
+        email=request.form['email']
+        date=request.form['date']
+        occupation=request.form['occupation']
+        # print(first_name)
+
     return(render_template("index.html"))
 
 
