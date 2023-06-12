f8cc0dac3806bbeab437d87a6498d34d1e90d867
sreejith <squeeky.mouse@gmail.com>
2023-06-12 11:03:16 +0530

    add submit confirmation via jinja in index

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy # sql/sqlite but interacts with flask!!!
 
 
@@ -33,6 +33,8 @@ def index():
                   email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        flash(f'{first_name}, your form was submitted successfully!', 'success')
 
     return(render_template("index.html"))
 
